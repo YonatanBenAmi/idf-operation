ae034ebefce3bd403a68f47d99e7d1a5584c0db7
Dan <danhemed123@gmail.com>
2025-05-21 11:57:28 +0300

    buliding the of beginning of the code structure

--- IDF-Operation/obj/Debug/net8.0/IDF-Operation.AssemblyInfo.cs
+++ IDF-Operation/obj/Debug/net8.0/IDF-Operation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IDF-Operation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5a061db03ff54bd1396ee59bd5da641fd392813")]
 [assembly: System.Reflection.AssemblyProductAttribute("IDF-Operation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IDF-Operation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
