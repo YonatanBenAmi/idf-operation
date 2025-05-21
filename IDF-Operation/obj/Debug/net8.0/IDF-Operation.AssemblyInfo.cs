0da481d0e18b81bd78a8bbfa14009214b6ebce98
Dan <danhemed123@gmail.com>
2025-05-21 15:59:15 +0300

    Building the beginning of the code structure of 'Main'.

--- IDF-Operation/obj/Debug/net8.0/IDF-Operation.AssemblyInfo.cs
+++ IDF-Operation/obj/Debug/net8.0/IDF-Operation.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IDF-Operation")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3b25df33a1b1d649c49c36af91a13788995782f4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5419e7b50faf7a454c146d6220955cf88300bcb5")]
 [assembly: System.Reflection.AssemblyProductAttribute("IDF-Operation")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IDF-Operation")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
