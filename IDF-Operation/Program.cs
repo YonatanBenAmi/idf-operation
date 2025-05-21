da8a13ff867ca4385d125bf8d7d88cead47403c3
Yonatan Ben Ami <yba6081@gmail.com>
2025-05-21 11:56:26 +0300

    Add: idf class

--- IDF-Operation/Program.cs
+++ IDF-Operation/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace IdfOperation
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
