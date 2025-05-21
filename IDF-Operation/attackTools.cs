1554d1c04fbf1862486d9c7966bb2ac5c29cb7e0
Yonatan Ben Ami <yba6081@gmail.com>
2025-05-21 15:51:18 +0300

    add: 2 basic classes

---
+++ IDF-Operation/attackTools.cs
@@ -0,0 +1,16 @@
+namespace IdfOperation.Models
+{
+    public class AttackTool : StrikeOption
+    {
+        public StrikeOption F16 = new StrikeOption("F16", 8, 10427, ["Terrorist", "Buildings", "Underground"]);
+        public StrikeOption Zik = new StrikeOption("Zik", 3,145, ["Terrorist", "Buildings"]);
+        public StrikeOption Artillery = new StrikeOption("Artillery", 40, 1400, ["Buildings"]);
+        
+
+        public AttackTool(string toolName, int capacity,  double fuel, List<string> typeTarget) : base(toolName, capacity, fuel, typeTarget)
+        {
+            
+        }
+
+    }
+}
