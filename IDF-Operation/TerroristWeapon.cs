d6eacd85a0ddc98f629eedbc3d7f45afac219113
Dan <danhemed123@gmail.com>
2025-05-25 17:40:45 +0300

    new week

--- IDF-Operation/TerroristWeapon.cs
+++ IDF-Operation/TerroristWeapon.cs
@@ -9,17 +9,29 @@ namespace IdfOperation.Models
 {
     class TerroristWeapon
     {
-        public string Name;
+        public string Knife;
         public string Gun;
         public string M16;
         public string AK47;
 
-        public TerroristWeapon(string name, string gun, string m16, string aK47)
+        public TerroristWeapon(string knife, string gun, string m16, string aK47)
         {
-            Name = name;
+            Knife = knife;
             Gun = gun;
             M16 = m16;
             AK47 = aK47;
+        }
+
+        public Dictionary<string, int> WeaponPoint()
+        {
+            Dictionary<string, int> weaponPoint = new Dictionary<string, int>();
+
+            weaponPoint["knife"] = 1;
+            weaponPoint["gun"] = 2;
+            weaponPoint["m16"] = 3;
+            weaponPoint["ak47"] = 3;
+
+            return weaponPoint;
         }
     }
 }
